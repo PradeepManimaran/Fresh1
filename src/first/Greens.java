eb9c9ee5658eb918e0459446f7ef56430135d537
PradeepManimaran <pradeepanbu1994@gmail.com>
2021-11-09 14:50:28 +0530

    Pradeepanbu

--- src/first/Greens.java
+++ src/first/Greens.java
@@ -7,9 +7,14 @@ public class Greens {
 	private void oracle() {
 		System.out.println("oracle course");
 	}
+	private void python() {
+		System.out.println("python course");
+	}
+	
 	public static void main(String[] args) {
 		Greens c = new Greens();
 		c.java();
 		c.oracle();
+		c.python();
 	}
 }
