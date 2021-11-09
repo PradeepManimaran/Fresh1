ec3d5bd563eb1370b362c4a97ee0366279854cfa
PradeepManimaran <pradeepanbu1994@gmail.com>
2021-11-09 13:35:41 +0530

    PDP

---
+++ src/first/Greens.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Greens {
+	private void java() {
+		System.out.println("java course");
+	}
+	private void oracle() {
+		System.out.println("oracle course");
+	}
+	public static void main(String[] args) {
+		Greens c = new Greens();
+		c.java();
+		c.oracle();
+	}
+}
---
+++ src/learning/Programs.java
@@ -0,0 +1,15 @@
+package learning;
+
+public class Programs {
+	private void java() {
+		System.out.println("Java Software");
+	}
+	private void python() {
+		System.out.println("PYTHON Software");
+	}
+	public static void main(String[] args) {
+		Programs p = new Programs();
+		p.java();
+		p.python();
+	}
+}
